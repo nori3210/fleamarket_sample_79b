c9e7b19bad420c19b0e666e1a9552753d983cbac
nori3210 <aiueoka3210@gmail.com>
2020-11-05 23:40:32 +0900

    routingはresourcesベースで修正
    routingはresourcesベースで修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-
-
-  get 'buyers/index'
-  get 'buyers/done'
-  get 'buyers/delivery'
- 
-
   devise_for :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'homes#index'
+  root to: 'homes#index'
+    resources :buyers do
+      collection do
+        get 'done'
+        get 'delivery'
+      end
+    end
 end
+
 
