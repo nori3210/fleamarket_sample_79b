9d2b3909576b7858adfab523dcdd3305a992b11d
nori3210 <aiueoka3210@gmail.com>
2020-11-07 21:03:42 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,15 @@ Rails.application.routes.draw do
   devise_for :users
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'homes#index'
-    resources :buyers do
-      collection do
-        get 'done'
-        get 'delivery'
-      end
-    end
+  
   root 'homes#index'
   resources :items
-  
+  resources :buyers do
+    collection do
+      get 'done'
+      get 'delivery'
+    end
+  end
 end
 
 
