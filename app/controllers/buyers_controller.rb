db74c52e242c99e4de659d8d6e554b86d6d68748
nori3210 <aiueoka3210@gmail.com>
2020-11-09 16:32:56 +0900

    修正

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -10,4 +10,3 @@ class BuyersController < ApplicationController
   end
 end
 
-test
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'items#index'
+  resources :buyers do
+    collection do
+      get 'done'
+      get 'delivery'
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
