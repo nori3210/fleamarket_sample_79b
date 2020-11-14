41719445d161903fa5673e318b7848aad2b0ebd7
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2020-11-14 17:25:55 +0900

    マイページの作成

--- Gemfile
+++ Gemfile
@@ -74,7 +74,8 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'jquery-rails'
 gem 'fog-aws'
-
+gem 'payjp'
+gem 'dotenv-rails'
 
 group :production do
   gem 'unicorn'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'card/new'
+  get 'card/show'
   root 'homes#index'
   resources :items
-  
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
+  resources :users
 end
