cb74165fd39d67e02a153d026cbc8b7c663e8520
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2020-11-14 20:17:52 +0900

    変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'card/new'
-  get 'card/show'
+
   root 'homes#index'
   resources :items
   resources :card, only: [:new, :show] do
     collection do
       post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
-      post 'delete', to: 'card#delete'
+      post 'delete', to: 'card#destroy'
     end
   end
   resources :users
