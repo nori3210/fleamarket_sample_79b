8dc64fbd681aa8755f9a9a8bd59fbff72bc81d72
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2020-11-15 11:56:29 +0900

    users_delete

--- config/routes.rb
+++ config/routes.rb
@@ -12,16 +12,7 @@ Rails.application.routes.draw do
   resources :users
 
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  root 'homes#index'
-  resources :items
-  resources :buyers do
-    collection do
-      get 'done'
-      get 'delivery'
-    end
-  end
+
 
 end
 
