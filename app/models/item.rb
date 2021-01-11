95b1df2e819980c19a4eeeba85ff7d86a51dc405
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2021-01-11 15:48:19 +0900

    商品出品画像
    画像投稿をしないまま出品ができる状態だったのでバリデーションをかけました

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,14 @@ class Item < ApplicationRecord
     validates :estimated_shipping_date_id
     validates :price
     validates :trading_status
+    validates :item_images, presence: true
   end
+
+  # validate :require_any_item_image
+  # def require_any_item_image
+  #   errors.add(:base, :no_item_image) if item_images.blank?
+  # end
+
   belongs_to :brand, optional: true
   belongs_to :user
   belongs_to :category, optional: true
