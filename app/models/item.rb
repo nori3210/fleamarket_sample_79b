b95040fa675d56ce7c8edb97200ad44fc80ff72c
nori3210 <aiueoka3210@gmail.com>
2021-01-11 22:01:29 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,10 +21,10 @@ class Item < ApplicationRecord
     validates :trading_status
   end
   belongs_to :brand, optional: true
-  belongs_to :user
   belongs_to :category, optional: true
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
 
-
+  belongs_to :user
+  has_one :buyer
 end
--- db/migrate/20201219121540_addresses.rb
+++ db/migrate/20201219121540_addresses.rb
@@ -1,15 +1,14 @@
 class Addresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-      t.integer "postal_code"
-      t.string "prefecture"
-      t.text "city"
-      t.text "house_number"
-      t.text "building"
-      t.integer "telephone"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.integer "user_id"
+      t.integer :postal_code
+      t.integer :prefecture
+      t.text :city
+      t.text :house_number
+      t.text :building
+      t.integer :telephone
+      t.timestamps
+      t.integer :user_id
     end
   end
 end
